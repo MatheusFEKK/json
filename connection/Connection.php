d4160fa156c6c9195f2251c28027ba3dc8dbc349
Matheus Felipe <matheusfelipelazarini4@gmail.com>
2024-12-07 01:46:38 -0300

    Cadastro e tabela retornando com dados do banco via JSON

--- connection/Connection.php
+++ connection/Connection.php
@@ -5,10 +5,8 @@ class Database extends mysqli
 {
     function __construct() 
     {
-        parent::__construct('localhost','root', '', 'db_api', 3306);
+        parent::__construct('localhost','root', '', 'db_json', 3308);
         $this->set_charset('utf8');
         $this->connect_error == NULL ? 'Connection Granted with the Database' : die('Connection Severed with the Database'); 
     }
-
-
 }
--- index.php
+++ index.php
@@ -8,16 +8,16 @@ switch ($_SERVER['REQUEST_METHOD']) {
             echo json_encode(Cliente::getWhere($_GET['id'])); // É usado para transformar o PHP em JSON (saída)
         } else 
         {
-            echo json_encode(Cliente::getAll());
+            $json = json_encode(Cliente::getAll());
+            
         }
         break;
 
     case 'POST':
-        $data = json_decode(file_get_contents('php://input')); // É usado para trasnformar JSON em PHP (entrada)
-
-        if ($data != NULL) 
+        // $data = json_decode(file_get_contents('php://input')); // É usado para trasnformar JSON em PHP (entrada)
+        if (isset($_POST['email']) && isset($_POST['nome'])) 
         {
-            if (Cliente::insert($data->nome, $data->email))
+            if (Cliente::insert($_POST['nome'], $_POST['email']))
             {
                 http_response_code(200);
             }else 
@@ -35,6 +35,20 @@ switch ($_SERVER['REQUEST_METHOD']) {
             if ($data != NULL)
             {
                 if (Cliente::update($data->id, $data->nome, $data->email))
+                {
+                    http_response_code();
+                }else {
+                    http_response_code(400);
+                }
+            }else {
+                http_response_code(405);
+            }
+
+        case 'DELETE':
+            $data = json_decode(file_get_contents('php://input'));
+            if ($data != NULL)
+            {
+                if (Cliente::delete($data->id))
                 {
                     http_response_code();
                 }else {
@@ -62,24 +76,49 @@ switch ($_SERVER['REQUEST_METHOD']) {
 </head>
 
 <body>
-    <div class="container">
-        <div class="box">
+    <div class="container d-flex justify-content-center">
+        <div class="box ">
             <h1>LOCALIZAR</h1>
             <form action="index.php" method="get">
                 <input type="text" value=""><br>
                 <input type="text" name="id" placeholder="ID"><br>
-                <button class="btn btn-primary" style="float:right;">ENVIAR</button>
+                <button class="btn btn-primary float-end">ENVIAR</button>
             </form>
         </div>
+            <div class="p-5"></div>
         <div class="box2">
             <h1>CADASTRO</h1>
-            <form action="index.php" method="post">
-                <input type="text" name="nome" placeholder="ID"><br>
-                <input type="text" name="email" placeholder="ID"><br><br>
-                <button class="btn btn-primary" style="float:right;">ENVIAR</button>
+            <form action="" method="POST">
+                <input type="text" name="nome" placeholder="NOME"><br>
+                <input type="text" name="email" placeholder="EMAIL"><br>
+                <button class="btn btn-primary float-end">ENVIAR</button>
             </form>
         </div>
+        </div>
+       
+       <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nome</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">MODIFICAR</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php 
+            foreach ($jsondecode = json_decode($json) as $users){
+                echo "<tr>
+                <th scope='row'>$users->id</th>
+                <td>$users->nome</td>
+                <td>$users->email</td>
+                <td><a class='btn btn-primary' href='?id=$users->id'>EDITAR</a>
+                <a class='btn btn-danger' href='?delete=$users->id'>DELETAR</a>
+                </tr>";
+            }
+            ?>
+  </tbody>
+</table>
     </div>
 </body>
-
 </html>
